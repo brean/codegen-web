cbb9d4f992f68944b4906e478a9bb39d1a9259b7
Andreas Bresser <andreas.bresser@dfki.de>
2024-07-22 16:32:51 +0200

    basic svelteflow app and todo

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# required next steps
+- read code from yaml (drag-and-drop/"upload")
+- render positions using dagree
+- create new classes, variables, ...
+
+# Ideas for future features
+- Include a text-editor (monacco)
+  - select language (python/yaml/...)
+- export AST to other languages
+- python CLI to create code
+  - use pyscript to generate code (maybe to expensive?)
---
+++ src/lib/components/BaseFlow.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import { writable } from 'svelte/store';
+    import {
+      SvelteFlow,
+      Controls,
+      Background,
+      BackgroundVariant,
+      MiniMap
+    } from '@xyflow/svelte';
+   
+    // import styles for Svelte Flow
+    import '@xyflow/svelte/dist/style.css';
+   
+    // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+    const nodes = writable([
+      {
+        id: '1',
+        type: 'input',
+        data: { label: 'Input Node' },
+        position: { x: 0, y: 0 }
+      },
+      {
+        id: '2',
+        type: 'default',
+        data: { label: 'Node' },
+        position: { x: 0, y: 150 }
+      }
+    ]);
+   
+    // same for edges
+    const edges = writable([
+      {
+        id: '1-2',
+        type: 'default',
+        source: '1',
+        target: '2',
+        label: 'Edge Text'
+      }
+    ]);
+   
+    const snapGrid: [width: number, height: number] = [25, 25];
+    let innerHeight = 500;
+  </script>
+   
+  <svelte:window bind:innerHeight={innerHeight} />
+
+  <div style:height={innerHeight + 'px'}>
+    <SvelteFlow
+      {nodes}
+      {edges}
+      {snapGrid}
+      fitView
+      on:nodeclick={(event) => console.log('on node click', event.detail.node)}
+    >
+      <Controls />
+      <Background variant={BackgroundVariant.Dots} />
+      <MiniMap />
+    </SvelteFlow>
+  </div>
